4fc2c158994d2a41eefc0df325ce6c99c42c3e2b
RiceCode <RiceCode@users.noreply.github.com>
2020-12-08 14:55:54 +0800

    Compatible with 3.5 and 3.7

--- ContainerOptimizerLite.py
+++ ContainerOptimizerLite.py
@@ -15,12 +15,10 @@ warnings.filterwarnings("ignore")
 
 initStart = Init()
 initPr = initStart.Prompt()
-xLcl = initPr[0]
-x20min = initPr[1]
-x20 = initPr[2]
-x40 = initPr[3]
-x40HC = initPr[4]
-x45 = initPr[5]
+x20 = initPr[0]
+x40 = initPr[1]
+x40HC = initPr[2]
+x45 = initPr[3]
 
 print("Note: This program will give a 'Simulation Complete!' status if it ran successfully.")
 print("      If it exit, it means the program did not complete successfully.")
@@ -29,8 +27,6 @@ print("")
 
 print("===The following containers are being used:===")
 print("Note: 0CBM = container not being used.")
-print("   LCL is activated with 1 CBM each")
-print("   20' Container minimum is: ", x20min, "CBM")
 print("   20' Container: ", x20, "CBM")
 print("   40' Container: ", x40, "CBM")
 print("   40HC Container: ", x40HC, "CBM")
@@ -43,38 +39,36 @@ print("")
 #set up:
 wb = openpyxl.load_workbook('readLite.xlsx')
 sheet = wb.get_sheet_by_name('Sheet')
-rowCount = sheet.max_row
-
-print(rowCount)
 
 #x = input("Finished looking at xlsx").lower()
 
 
 
 #Set up headers:
-sheet.cell(row=1, column=2).value = "LCL"
-sheet.cell(row=1, column=3).value = "20'"
-sheet.cell(row=1, column=4).value = "40'"
-sheet.cell(row=1, column=5).value = "40HC"
-sheet.cell(row=1, column=6).value = "45'"
-sheet.cell(row=1, column=7).value = "Utilization"
+sheet.cell(row=1, column=2).value = "20'"
+sheet.cell(row=1, column=3).value = "40'"
+sheet.cell(row=1, column=4).value = "40HC"
+sheet.cell(row=1, column=5).value = "45'"
+sheet.cell(row=1, column=6).value = "Utilization"
+
+#x = input("About to start doing sim").lower()
+
+#loop and input to right.
+rowCount = sheet.max_row+1
 
 
-
-
-for rowNum in range(2, rowCount+1):  # skip the first row
+for rowNum in range(2, rowCount):  # skip the first row
     #print("Currently doing : ", rowNum)
     CBM = sheet.cell(row=rowNum, column=1).value
     if CBM == 0:
         break
-    cntrOpt = Optimization(xLcl, x20min, x20, x40, x40HC, x45, CBM)
+    cntrOpt = Optimization(x20, x40, x40HC, x45, CBM)
     cntrRslt = cntrOpt.LP()
-    sheet.cell(row=rowNum, column=2).value = cntrRslt[0] #LCL
-    sheet.cell(row=rowNum, column=3).value = cntrRslt[1] #20
-    sheet.cell(row=rowNum, column=4).value = cntrRslt[2] #40
-    sheet.cell(row=rowNum, column=5).value = cntrRslt[3] #40HC
-    sheet.cell(row=rowNum, column=6).value = cntrRslt[4] #45
-    sheet.cell(row=rowNum, column=7).value = cntrRslt[5] #Utilization
+    sheet.cell(row=rowNum, column=2).value = cntrRslt[0] #20
+    sheet.cell(row=rowNum, column=3).value = cntrRslt[1] #40
+    sheet.cell(row=rowNum, column=4).value = cntrRslt[2] #40HC
+    sheet.cell(row=rowNum, column=5).value = cntrRslt[3] #45
+    sheet.cell(row=rowNum, column=6).value = cntrRslt[4] #Utilization
 
     #x = input("Finished with a sim").lower()
     del cntrRslt
--- Init.py
+++ Init.py
@@ -1,31 +1,15 @@
 
-
 class Init:
     def __init__(self):
-        self.useLcl = True
-        self.use20 = True
-        self.use40 = True
-        self.use40HC = True
-        self.use45 = True
+        self.use20 = "True"
+        self.use40 = "True"
+        self.use40HC = "True"
+        self.use45 = "True"
 
-        self.sizeLcl = 1
         self.size20 = 27
         self.size40 = 57
         self.size40HC = 67
         self.size45 = 76
-        self.minLoad20 = 20
-
-
-    def CheckNotUsed(self, containerType, containerSize):
-        print(containerType, containerSize)
-        
-        if containerType == "False":
-            #print("Im returning 0")
-            return 0
-        else:
-            #print("Im returning containerSize")
-            return containerSize
-
 
 
 
@@ -36,7 +20,6 @@ class Init:
                 break
             if decision == "help":
                     print("Default means the following will be allowed:")
-                    print("LCL = %d"  %self.sizeLcl)
                     print("20' = %d"  %self.size20)
                     print("40' = %d"  %self.size40)
                     print("40HC = %d"  %self.size40HC)
@@ -44,40 +27,40 @@ class Init:
             if decision == "no":
                     change = input("Would you like to change Container CBM or Container Type Allowed?").lower()
                     if(change.lower() == "container cbm"):
-                        self.minLoad20 = int(input("What's the minimum loadability for 20'? Anything lower will be in LCL"))
                         self.size20 = int(input("What is the maximum CBM you would like for your 20'"))
                         self.size40 = int(input("What is the maximum CBM you would like for your 40'"))
                         self.size40HC = int(input("What is the maximum CBM you would like for your 40HC"))
                         self.size45 = int(input("What is the maximum CBM you would like for your 45'"))
                         print("The default values has been updated.")
                     if(change.lower() == "container type allowed"):
-                        self.useLcl = input("Should LCL be used? Please reply True or False")
-                        print("use LCL = " + self.useLcl)
-                        
-                        self.use20 = input("Should 20' be used? Please reply True or False")
-                        print("use 20 = " + self.use20)
-                        
-                        self.use40 = input("Should 40' be used? Please reply True or False")
-                        print("use 40 = " + self.use40)
-                        
-                        self.use40HC = input("Should 40HC be used? Please reply True or False")
-                        print("use 40HC = " + self.use40HC)
-                                        
-                        self.use45 = input("Should 45' be used? Please reply True or False")
-                        print("use 45 = " + self.use45)
-
-                        #Container being used:
-                        self.sizeLcl = self.CheckNotUsed(self.useLcl, self.sizeLcl)
-                        self.size20 = self.CheckNotUsed(self.use20, self.size20)
-                        self.size40 = self.CheckNotUsed(self.use40, self.size40)
-                        self.size40HC = self.CheckNotUsed(self.use40HC, self.size40HC)
-                        self.size45 = self.CheckNotUsed(self.use45, self.size45)
-
+                        self.use20 = input("Should 20' be used? Please reply True or False)")
+                        print("use20 = " + self.use20)
+                        self.use40 = input("Should 40' be used? Please reply True or False)")
+                        print("use40 = " + self.use40)
+                        self.use40HC = input("Should 40HC be used? Please reply True or False)")
+                        print("use40HC = " + self.use40HC)
+                        self.use45 = input("Should 45' be used? Please reply True or False)")
+                        print("use45 = " + self.use45)
                         print("The default values has been updated.")
 
-            return(self.sizeLcl, self.minLoad20, self.size20, self.size40, self.size40HC, self.size45)
+
+            #Container being used:
+            self.size20 = self.CheckNotUsed(self.use20, self.size20)
+            self.size40 = self.CheckNotUsed(self.use40, self.size40)
+            self.size40HC = self.CheckNotUsed(self.use40HC, self.size40HC)
+            self.size45 = self.CheckNotUsed(self.use45, self.size45)
+
+        return(self.size20, self.size40, self.size40HC, self.size45)
 
 
+    def CheckNotUsed(self, containerType, containerSize):
+        print("Checking ", containerType, containerSize)
+        if containerType == "False":
+            #print("Im returning 0")
+            return 0
+        else:
+            #print("Im returning containerSize")
+            return containerSize
 
 
 
